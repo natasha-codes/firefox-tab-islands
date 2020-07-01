041d7633a0755d1cedc6aeeacf5b7e1aea32bda5
Nathan Shelly <nsnshelly@gmail.com>
2020-06-30 21:39:43 -0500

    Remove unneeded `.js` files

--- src/background.js
+++
@@ -1,6 +0,0 @@
-browser.tabs.onCreated.addListener(async (_) => {
-  console.log("new tab!")
-  const identities = await browser.contextualIdentities.query({});
-
-  identities.forEach(id => console.log(id))
-})

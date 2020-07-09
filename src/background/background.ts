d61f5594a0a1e861ce31f8ae4570095ceb6a882a
Nathan Shelly <nsnshelly@gmail.com>
2020-07-08 22:14:59 -0500

    Add toolbar icon (launches options page)

--- src/background/background.ts
+++ src/background/background.ts
@@ -5,6 +5,13 @@ const main = async () => {
   await SettingsManager.shared.attach()
 
   IslandRouter.shared.attach()
+
+  // launch options page when user clicks button in toolbar
+  // ref - https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Add_a_button_to_the_toolbar
+  browser.browserAction.onClicked.addListener(() =>
+    // ref - https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Implement_a_settings_page
+    browser.runtime.openOptionsPage(),
+  )
 }
 
 main()
